46c5db9666b08a619af0dd7464f2bf7d55a29d6a
Admin <sorokivskyi777@gmail.com>
2023-07-30 11:33:06 +0300

    lesson1 resolve start db in docker, db connect

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,19 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserController } from './user/user.controller';
 import { UserModule } from './user/user.module';
 import { UserService } from './user/user.service';
 import { PetModule } from './pet/pet.module';
+import { TypeOrmConfiguration } from './config/database/type-orm-configuration';
 
 @Module({
-  imports: [UserModule, PetModule],
+  imports: [
+    TypeOrmModule.forRootAsync(TypeOrmConfiguration.config),
+    UserModule,
+    PetModule,
+  ],
   controllers: [AppController, UserController],
   providers: [AppService, UserService],
 })
